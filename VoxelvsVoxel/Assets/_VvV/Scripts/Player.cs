83de4b7570a336f98766a15ddf879ca33a6517e2
trinplaytika <minhn@playtika.com>
2019-11-20 17:33:46 -0500

    Init project

---
+++ VoxelvsVoxel/Assets/_VvV/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    private Vector3 direction = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoMovement();
+    }
+
+    public void DoMovement()
+    {
+        direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
